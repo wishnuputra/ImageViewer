0284323c8bf16b839548bf6c021f27085832b786
wishnuputra <wishnuputra.dhanu@gmail.com>
2024-03-21 21:28:34 +0100

    while loop

--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,17 @@
-print("Enter todo: ")
+todos = []
+
+while True:
+    user_action = input("Type add, show, or exit: ")
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            [print(todo) for todo in todos]
+        case 'exit':
+            break
+        case _:
+            print("Not a valid input, please type again!!!")
+
+print("Bye")
